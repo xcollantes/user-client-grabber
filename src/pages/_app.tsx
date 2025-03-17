f93636c92e6d467e31ae889bb86eedf39e924ebf
Xavier <collantes.xavier@gmail.com>
2025-03-17 11:38:19 -0700

    Add queryclient
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  // Create a query client instance that persists for the lifetime of the
+  // component. 
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <main className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Component {...pageProps} />
+      </main>
+    </QueryClientProvider>
+  );
 }
